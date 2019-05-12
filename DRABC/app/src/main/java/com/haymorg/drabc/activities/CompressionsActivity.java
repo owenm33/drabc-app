dae205e39a08dd19927b591daa9a00b41addea30
d4vidh4y <49356964+d4vidh4y@users.noreply.github.com>
2019-05-12 22:13:14 +1000

    added button to play cpr tempo

--- DRABC/app/src/main/java/com/haymorg/drabc/activities/CompressionsActivity.java
+++ DRABC/app/src/main/java/com/haymorg/drabc/activities/CompressionsActivity.java
@@ -2,24 +2,37 @@ package com.haymorg.drabc.activities;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.haymorg.drabc.R;
 import com.haymorg.drabc.databinding.ActivityCompressionsBinding;
 
 public class CompressionsActivity extends AppCompatActivity {
 
     private ActivityCompressionsBinding binding;
-
+    private MediaPlayer cprSoundMediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_compressions);
         getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        cprSoundMediaPlayer = MediaPlayer.create(this, R.raw.play_cpr);
+        cprSoundMediaPlayer.setLooping(true);
+//        final Button playPlay_Cpr = (Button) this.findViewById(R.id.play_cpr);
     }
 
+    public void onPlay(View v) {
+        cprSoundMediaPlayer.start();
+    }
+    public void onStop(View v) {
+        cprSoundMediaPlayer.stop();
+    }
     public void onClose(View v) {
         finish();
     }
--- DRABC/app/src/main/java/com/haymorg/drabc/activities/MainActivity.java
+++ DRABC/app/src/main/java/com/haymorg/drabc/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.haymorg.drabc.activities;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -22,11 +23,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         userDetails = getSharedPreferences("USER", MODE_PRIVATE);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
     }
 
+
+
     /* Should we have @Override protected void onNewIntent(Intent intent) here?
-    ** And set android:launchMode="singleTop" for MainActivity in the manifest?
-    **/
+     ** And set android:launchMode="singleTop" for MainActivity in the manifest?
+     **/
 
     public void onDanger(View v) {
         startActivity(new Intent(MainActivity.this, DangerActivity.class));
