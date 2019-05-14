41e0609fa7a2379602906cffc9d1f7e57e0286dd
owenm33 <32035380+owenm33@users.noreply.github.com>
2019-05-15 06:45:19 +1000

    Create constructConditionJSONArray.py

---
+++ helpers/constructConditionJSONArray.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 15 05:32:33 2019
+
+@author: owenm
+"""
+
+import os, json
+
+path = os.getcwd()
+
+json_files = [pos_json for pos_json in sorted(os.listdir(path)) if pos_json.endswith('.json')]
+conditions = []
+
+for index, js in enumerate(json_files):
+    with open(os.path.join(path, js), encoding="utf8") as json_file:
+        json_text = json.load(json_file)
+#        conditions.append({
+#                "id": js.split()[2],
+#                "name": json_text['Name'],
+#                "treatment": json_text['TreatmentDescription']})
+        condition = {}
+        condition['id'] = js.split()[2]
+        condition['name'] = json_text['Name']
+        condition['treatment'] = json_text['TreatmentDescription']
+        conditions.append(condition)
+   
+
+
+with open('conditions.json', 'w') as outfile:
+    json.dump(conditions, outfile, indent=4)
