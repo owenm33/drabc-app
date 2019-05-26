0c4dd813847a89210e19fb5984599d93a4d8461a
d4vidh4y <49356964+d4vidh4y@users.noreply.github.com>
2019-05-26 18:41:20 +1000

    Revert "Last meetings changes"
    This reverts commit c17134f19fb859619381f134500c05f9193f716d.

--- DRABC/app/src/main/java/com/haymorg/drabc/activities/CompressionsActivity.java
+++ DRABC/app/src/main/java/com/haymorg/drabc/activities/CompressionsActivity.java
@@ -9,46 +9,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-
 import com.haymorg.drabc.R;
 import com.haymorg.drabc.databinding.ActivityCompressionsBinding;
-
-import pl.droidsonroids.gif.GifDrawable;
 
 public class CompressionsActivity extends AppCompatActivity {
 
     private ActivityCompressionsBinding binding;
-//    private MediaPlayer cprSoundMediaPlayer;
-    private GifDrawable cprGif;
+    private MediaPlayer cprSoundMediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_compressions);
         getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        cprGif = binding.cprGif;
 
+        cprSoundMediaPlayer = MediaPlayer.create(this, R.raw.play_cpr);
+        cprSoundMediaPlayer.setLooping(true);
+//        final Button playPlay_Cpr = (Button) this.findViewById(R.id.play_cpr);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        cprGif.startAnimation();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        cprGif.startAnimation();
-    }
     public void onPlay(View v) {
-
-    }
-    public void onDemo(View v) {
-        binding.cprgif.setVisibility(View.VISIBLE);
-        binding.compressionsBody.setVisibility(View.INVISIBLE);
+        cprSoundMediaPlayer.start();
     }
     public void onStop(View v) {
-
+        cprSoundMediaPlayer.stop();
     }
     public void onClose(View v) {
         finish();
