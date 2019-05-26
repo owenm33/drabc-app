c17134f19fb859619381f134500c05f9193f716d
d4vidh4y <49356964+d4vidh4y@users.noreply.github.com>
2019-05-26 18:39:00 +1000

    Last meetings changes

--- DRABC/app/src/main/java/com/haymorg/drabc/activities/CompressionsActivity.java
+++ DRABC/app/src/main/java/com/haymorg/drabc/activities/CompressionsActivity.java
@@ -9,29 +9,46 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+
 import com.haymorg.drabc.R;
 import com.haymorg.drabc.databinding.ActivityCompressionsBinding;
+
+import pl.droidsonroids.gif.GifDrawable;
 
 public class CompressionsActivity extends AppCompatActivity {
 
     private ActivityCompressionsBinding binding;
-    private MediaPlayer cprSoundMediaPlayer;
+//    private MediaPlayer cprSoundMediaPlayer;
+    private GifDrawable cprGif;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_compressions);
         getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        cprGif = binding.cprGif;
 
-        cprSoundMediaPlayer = MediaPlayer.create(this, R.raw.play_cpr);
-        cprSoundMediaPlayer.setLooping(true);
-//        final Button playPlay_Cpr = (Button) this.findViewById(R.id.play_cpr);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cprGif.startAnimation();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cprGif.startAnimation();
+    }
     public void onPlay(View v) {
-        cprSoundMediaPlayer.start();
+
+    }
+    public void onDemo(View v) {
+        binding.cprgif.setVisibility(View.VISIBLE);
+        binding.compressionsBody.setVisibility(View.INVISIBLE);
     }
     public void onStop(View v) {
-        cprSoundMediaPlayer.stop();
+
     }
     public void onClose(View v) {
         finish();
